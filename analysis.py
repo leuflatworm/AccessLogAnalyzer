126ec5cf714342d1f50581e6548213bdbc9e191b
leu <leu@localhost.localdomain>
2020-05-22 13:27:23 +0900

    update

--- analysis.py
+++ analysis.py
@@ -3,56 +3,7 @@
 month_dict = {"Jan":"01", "Feb":"02", "Mar":"03", "Apr":"04", "May":"05", "Jun":"06", "Jul":"07", "Aug":"08", "Sep":"09", "Oct":"10", "Nov":"11", "Dec":"12"}
 
 
-def host(Files, sd, ed, ex):
-    access_list = {}
-
-    for file in Files:
-        f = open("targets/" + file)
-        line = f.readline()
-
-
-        while line:
-            elm = line.split(" ")
-            IP = elm[0]
-            date = elm[4]
-            dd = date[1:3]
-            mm = month_dict[date[4:7]]
-            yyyy = date[8:12]
-
-            cd = yyyy + mm + dd
-
-            line = f.readline()
-            if int(sd) < int(cd) and int(cd) < int(ed):
-                print(IP)
-
-                if IP in access_list:
-                    n = access_list[IP]
-                    n = n+1
-                    access_list[IP] = n
-
-                else:
-                    access_list[IP] = 1
-
-        f.close()
-
-    access_sorted = sorted(access_list.items(), key=lambda x:x[1], reverse=True)
-
-
-    w = open("exports/" + ex, mode = "w")
-
-    w.write("対象ファイル：")
-
-    for i in Files:
-        w.write(i + ",")
-
-    w.write(",解析時期" + sd + "〜" + ed + "\n")
-
-    for i in range(len(access_sorted)):
-        w.write(access_sorted[i][0] + "," + str(access_sorted[i][1]) + "\n")
-
-
-
-def time(Files, sd, ed, ex):
+def analysis(Files, sd, ed, ex):
 
     access_list = {}
 
@@ -68,20 +19,28 @@ def time(Files, sd, ed, ex):
             dd = date[1:3]
             mm = month_dict[date[4:7]]
             yyyy = date[8:12]
+            hh = date[13:15]
 
-            cd = yyyy + mm + dd
+            cd = yyyy + mm + dd + hh
 
             line = f.readline()
-            if int(sd) <= int(cd) and int(cd) <= int(ed):
-                print(cd)
+            if int(sd + "00") <= int(cd) and int(cd) <= int(ed + "23"):
 
                 if cd in access_list:
-                    n = access_list[cd]
-                    n = n+1
-                    access_list[cd] = n
+                    if IP in access_list[cd]:
+                        n = access_list[cd][IP]
+                        n = n+1
+                        access_list[cd][IP] = n
+
+                    else:
+                        access_list[cd][IP] = 1
 
                 else:
-                    access_list[cd] = 1
+                    access_list[cd] = {}
+                    access_list[cd][IP] = 1
+
+
+    access_time_sorted = sorted(access_list.items())
 
 
     w = open("exports/" + ex, mode = "w")
@@ -93,9 +52,20 @@ def time(Files, sd, ed, ex):
 
     w.write(",解析時期" + sd + "〜" + ed + "\n")
 
-    for i in access_list:
-            w.write(i + "," + str(access_list[i]) + "\n")
+    for i in range(len(access_time_sorted)):
+        print("-----------------------------------------------------------------------")
+        print(access_time_sorted[i][0])
+        access_sorted = sorted(access_time_sorted[i][1].items(), key=lambda x:x[1], reverse = True)
+        print(access_sorted)
+        w.write(access_time_sorted[i][0][:8] + ":" + access_time_sorted[i][0][8:10] + ":00~" + access_time_sorted[i][0][8:10] + ":59, ")
+        for j in range(len(access_sorted)):
+            print(access_sorted[j][0] + " : " + str(access_sorted[j][1]))
+            w.write(access_sorted[j][0] + ":" + str(access_sorted[j][1]) + ", ")
 
+        w.write("\n")
+
+
+    w.close()
 
 
 
--- main.py
+++ main.py
@@ -1,17 +1,23 @@
 import os
 import analysis
+import sys
 
 def main():
 
-    print("==========================================================")
-    print("=AccessLogAnalyzer                                       =")
-    print("=アクセスログの解析を行います。                          =")
-    print("=このプログラムはPython 3.7.6にて動作確認を行っています。=")
-    print("==========================================================")
+    print("============================================================================================")
+    print("|    AccessLogAnalyzer                                                                     |")
+    print("| アクセスログの解析を行います。                                                           |")
+    print("| このプログラムはPython 3.7.6にて動作確認を行っています。                                 |")
+    print("| コマンドラインオプションを指定しない場合は対話モードで起動します。                       |")
+    print("|                                                                                          |")
+    print("| 起動時に以下の様に指定した場合は結果のみ出力します。                                     |")
+    print("| python3 main.py f=FILE_NAME sd=START_DATE(yyyymmdd) ed=END_DATE(yyyymmdd) ex=EXPORT_FILE |")
+    print("============================================================================================")
 
     Filenames = [] #対象とするファイルのリスト
     sdate = "00000000"#集計を開始する日付
     edate = "99999999"#集計を終了する日付
+    ex_file = ""
 
     ##対象とするファイルの選択を行う
     xxx = "n"
@@ -32,11 +38,18 @@ def main():
             if ans == "y":
                 Filenames.extend(files)
 
+
         else:
-            print(files[select] + "でよろしいですか？[y/n]")
-            if input() == "y":
-                for i in select:
-                    Filenames.append(files[int(i)])
+            try:
+                for s in select:
+                    print(files[int(s)], end=" ")
+                    print("でよろしいですか？[y/n]")
+                    if input() == "y":
+                        for i in select:
+                            Filenames.append(files[int(i)])
+
+            except TypeError:
+                print("all又は半角英数字で回答してください。")
 
         for i in range(len(Filenames)):
             print(Filenames[i])
@@ -50,53 +63,90 @@ def main():
             elif ans == "n":
                 break
 
-    print("どちらの解析を行いますか？")
-    print("[t]各時間帯毎のアクセス件数を知りたい")
-    print("[h]リモートホスト別のアクセス件数知りたい")
-    analysisType = ""
-    ans = input()
-    if ans =="t":
-        analysisType = "t"
-    elif ans =="h":
-        analysisType = "h"
-
-
-
 
 
     ##期間の指定
-    print("集計を開始する日付を指定しますか？[y/n]")
-    ans = input()
-    if ans == "y":
-        print("集計を開始する日付をyyyymmddで入力してください(例：1970年1月1日 -> 19700101)")
-        sdate = input()
-
-    print("集計を終了する日付を指定しますか？[y/n]")
-    ans = input()
-    if ans == "y":
-        print("集計を終了する日付をyyyymmddで入力してください(例：1970年1月1日 -> 19700101)")
-        edate = input()
-
-    if int(sdate)>int(edate):
-        print("期間が不正です。")
-
-    print(sdate + "から" + edate + "の期間で集計を行います。")
-
-    print("出力ファイル名を指定しますか？[y/n]（指定しない場合はexport.csvとして出力します。）")
-    ans = input()
-
-    ex_file = "export.csv"
-    if ans == "y":
-        print("出力ファイル名を入力してください")
-        ex_file = input()
+    while True:
+        print("集計を開始する日付を指定しますか？[y/n]")
+        ans = input()
+        if ans == "y":
+            print("集計を開始する日付をyyyymmddで入力してください(例：1970年1月1日 -> 19700101)")
+            sdate = input()
+            break
+        elif ans == "n":
+            break
+        else:
+            print("[y/n]で回答してください。")
 
 
-    if analysisType == "t":
-        analysis.time(Filenames, sdate, edate, ex_file)
+    while True:
+        print("集計を終了する日付を指定しますか？[y/n]")
+        ans = input()
+        if ans == "y":
+            print("集計を終了する日付をyyyymmddで入力してください(例：1970年1月1日 -> 19700101)")
+            edate = input()
+            break
+        elif ans == "n":
+            break
+        else:
+            print("[y/n]で回答してください")
 
-    elif analysisType == "h":
-        analysis.host(Filenames, sdate, edate, ex_file)
+    try:
+        if int(sdate)>int(edate):
+            print("開始時期よりも終了時期が前になっています。（初期設定に戻します。）")
+            sdate = "00000000"
+            edate = "99999999"
+    except ValueError:
+        print("値が不正です。（初期設定に戻します。）")
+        sdate = "00000000"
+        edate = "99999999"
+
+    if sdate == "00000000" and edate == "99999999":
+        print("全期間で集計を行います。")
+    else:
+        print(sdate + "から" + edate + "の期間で集計を行います。")
+
+
+    ##出力ファイル指定
+    while True:
+        print("出力ファイル名を指定しますか？[y/n]（指定しない場合はexport.csvとして出力します。）")
+        ans = input()
+
+        if ans == "y":
+            print("出力ファイル名を入力してください")
+            ex_file = input()
+            break
+        elif ans == "n":
+            ex_file = "export.csv"
+            break
+        else:
+            print("[y/n]で回答してください。")
+
+
+
+    analysis.analysis(Filenames, sdate, edate, ex_file)
+
+
+def cmd(opt):
+    a = {"s":"00000000", "e":"99999999", "x":"export.csv"}
+    print("ok")
+    for o in opt[1:]:
+        s = o.split("=")
+        a[s[0]] = s[1]
+
+
+    Filenames = a["f"].split(",")
+    analysis.analysis(Filenames, a["s"], a["e"], a["x"])
+
+
+
+
+
 
 
 if __name__ == '__main__':
-    main()
+    args = sys.argv
+    if len(args) <= 1:
+        main()
+    else:
+        cmd(args)
